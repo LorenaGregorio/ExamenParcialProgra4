472343f36ac8f5960a36a95fdd66568a6e2e4e03
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-08-17 08:07:37 -0600

    Agregar archivos de proyecto.

---
+++ ModelosExamenParcialProgra4/Clientes/ClientesModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelosExamenParcialProgra4.Clientes
+{
+    public class ClientesModel
+    {
+        public int IdCliente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Nit { get; set; }
+        public int NumerodePedido { get; set; }
+        public string DescripcionPedido { get; set; }
+        public string FormadePago { get; set; }
+    }
+}
